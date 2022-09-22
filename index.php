eda649d227890d5cbbe49bc05e6786283bee2543
Slavik-Cider <103422738+slavikpas@users.noreply.github.com>
2022-09-22 12:10:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ class HelloWorld
         public function sayHello(DateTimeImutable $date): void
         {
                 echo 'Hello, 7' . $date->format('j. n. Y');
+                 echo 'Hel456lo, 1' . $date->format('j. n. Y'); echo 'Hel456lo, 1' . $date->format('j. n. Y'); echo 'Hel456lo, 1' . $date->format('j. n. Y');
         }
 }
 
